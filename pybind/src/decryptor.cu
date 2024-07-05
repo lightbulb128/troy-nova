8698aa9e2ee5af4960731cf89444356406fbd344
Lightbulb128 <lxq22@mails.tsinghua.edu.cn>
2024-07-05 13:04:18 +0800

    pybind: add bfv_decrypt_without_scaling_down

--- pybind/src/decryptor.cu
+++ pybind/src/decryptor.cu
@@ -21,6 +21,16 @@ void register_decryptor(pybind11::module& m) {
         .def("decrypt_new", [](const Decryptor& self, const Ciphertext& encrypted, MemoryPoolHandleArgument pool) {
             return self.decrypt_new(encrypted, nullopt_default_pool(pool));
         }, py::arg("encrypted"), MEMORY_POOL_ARGUMENT)
+
+        // bfv_decrypt_without_scaling_down(encrypted, destination, pool)
+        .def("bfv_decrypt_without_scaling_down", [](const Decryptor& self, const Ciphertext& encrypted, Plaintext& destination, MemoryPoolHandleArgument pool) {
+            self.bfv_decrypt_without_scaling_down(encrypted, destination, nullopt_default_pool(pool));
+        }, py::arg("encrypted"), py::arg("destination"), MEMORY_POOL_ARGUMENT)
+
+        // bfv_decrypt_without_scaling_down_new(encrypted, pool)
+        .def("bfv_decrypt_without_scaling_down_new", [](const Decryptor& self, const Ciphertext& encrypted, MemoryPoolHandleArgument pool) {
+            return self.bfv_decrypt_without_scaling_down_new(encrypted, nullopt_default_pool(pool));
+        }, py::arg("encrypted"), MEMORY_POOL_ARGUMENT)
     ;
 
 }
